c024e47b5a296461b1c916c234f5e35f15e50fa6
cmora <cmora@LAPTOP-457CD23L>
2023-03-01 14:44:50 +0100

    Versión 1

--- src/main/java/cuentas/Deposito.java
+++ src/main/java/cuentas/Deposito.java
@@ -11,13 +11,13 @@ public class Deposito {
         double saldoActual;
 
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        operativa_cuenta(cuenta1, 0);
+        operativa_cuenta(cuenta1);
     }
 
-    private static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
+    private static void operativa_cuenta(CCuenta cuenta1) {
         double saldoActual;
-        saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        saldoActual = cuenta1.getSaldo();
+        System.out.printf("El saldo actual es: %.2f €\n", saldoActual );
         try {
             cuenta1.retirar(2300);
         } catch (Exception e) {
